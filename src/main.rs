ecfe6b4746f99fac3662cc1e6ddc25e706d92fdf
Tobias <tobii12345@gmail.com>
2023-02-18 12:03:28 +0100

    fuege closure example hinzu

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod moo;
 mod trait_me;
+mod examples;
 
 fn main() {
     let f = moo::nested::foo();
     println!("{}", f);
     println!("Hello, world!");
+    print!("{:?}", examples::closure::closure_example(vec![0,81]));
 }
